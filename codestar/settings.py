accc2d0a631c9a1d2fd148f33a151800f004798d
Zealous242 <yasum.abbas137@gmail.com>
2026-09-05 18:46:03 +0100

    Change value of DEBUG in settings.py and update env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                     '127.0.0.1',]
